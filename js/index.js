f682029b78d6a3ad195f8bd38bc3ea4fa9d64042
unknown <mercury8862@gmail.com>
2016-11-03 12:51:29 +0800

    认证中心添加/删除地址js

--- js/index.js
+++ js/index.js
@@ -4,4 +4,56 @@ $(function(){
        $('.agree-protocol').show();
    });
 
+    //认证中心添加地址
+    $('.certi-main').delegate('.add-adress','click',function(){
+        var $this = $(this);
+        var areaBox = $this.parents('p').siblings('.my-area');
+        var prov = $this.parents('p').find('.prov').val();
+        var city = $this.parents('p').find('.city').val();
+        var dist = $this.parents('p').find('.dist').val();
+        var newStr = '';
+        var temp = '';
+        var flag = false;
+        if(prov){
+            newStr += prov;
+        }
+        if(city){
+            newStr += '-' + city;
+        }
+        if(dist){
+            newStr += '-' + dist;
+        }
+        //去重
+        if($('.my-area').find('span').length == 0){
+            flag = true;
+        }else{
+            $('.my-area').find('span').each(function(i){
+                var choosedArea = $(this).html().replace('<i>X</i>','');
+                if(newStr == choosedArea){
+                    flag = false;
+                }else{
+                    flag = true;
+                }
+            });
+        }
+
+        if(flag){
+            temp = '<span>'+newStr+'<i>X</i></span>';
+            areaBox.append(temp).removeClass('hide');
+        }
+
+
+    });
+    //认证中心移除地址
+    $('.certi-main').delegate('.my-area span i','click',function(){
+        var $this = $(this);
+        var areaBox = $this.parents('.my-area');
+        $this.parents('span').remove();
+        if(areaBox.find('span').length == 0){
+            areaBox.addClass('hide');
+        }
+
+    });
+
+
 });
